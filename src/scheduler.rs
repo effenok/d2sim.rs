b1fa19299ac274989420d774f7482a2ef1cf04da
effie <unknown@unknown.de>
2021-10-04 10:34:58 +0000

    [sim] scheduler preserves event order for simultaneous events
    * added event index to ensure order of simultaneous events
    * added test

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -40,25 +40,31 @@ pub enum EventType {
 struct ScheduledEvent
 {
     time: SimTime,
+    index: usize,
     event: EventType,
 }
 
 impl Ord for ScheduledEvent {
     fn cmp(&self, other: &Self) -> Ordering
     {
-        self.time.cmp(&other.time).reverse()
+        let mut ord = self.time.cmp(&other.time);
+        if let Ordering::Equal = ord {
+            ord = self.index.cmp(&other.index);
+        }
+
+        ord.reverse()
     }
 }
 
 impl PartialOrd for ScheduledEvent {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.time.cmp(&other.time).reverse())
+        Some(self.cmp(other))
     }
 }
 
 impl PartialEq for ScheduledEvent {
     fn eq(&self, other: &Self) -> bool {
-        self.time == other.time
+        self.time == other.time && self.index == other.index
     }
 }
 
@@ -74,12 +80,19 @@ pub struct Scheduler
     curr_time: SimTime,
     pub(crate) env: Environment,
     sim_status: SimStatus,
+    next_event: usize,
 }
 
 impl Scheduler
 {
     pub fn new() -> Self {
-        Scheduler { events: BinaryHeap::default(), curr_time: SimTime::default(), env: Environment::default(), sim_status: SimStatus::Ok}
+        Scheduler {
+            events: BinaryHeap::default(),
+            curr_time: SimTime::default(),
+            env: Environment::default(),
+            sim_status: SimStatus::Ok,
+            next_event: 0,
+        }
     }
 
     pub fn get_curr_time(&self) -> &SimTime {
@@ -117,11 +130,12 @@ impl Scheduler
         let time = self.curr_time + timedelta;
         let event = ScheduledEvent {
             time,
+            index: self.next_event,
             event: EventType::MsgSendEvent(
                 MessageSendEvent { sender, channel, message }
             ),
         };
-        self.events.push(event);
+        self.push_event(event);
     }
 
     pub fn send_msg(&mut self, sender: ComponentId, channel: ChannelId, message: Box<dyn Any>) {
@@ -130,39 +144,116 @@ impl Scheduler
 
     pub fn sched_receive_msg(&mut self, timedelta: SimTimeDelta, receiver: ComponentId, channel: ChannelId, message: Box<dyn Any>) {
         let time = self.curr_time + timedelta;
-        let event = ScheduledEvent { time, event: EventType::MsgRcvEvent(
-            MessageRcvEvent {channel, receiver, message}
-        )};
-        self.events.push(event);
+        let event = ScheduledEvent {
+            time,
+            index: self.next_event,
+            event: EventType::MsgRcvEvent(
+                MessageRcvEvent { channel, receiver, message }
+            ),
+        };
+        self.push_event(event);
     }
 
     pub fn sched_self_event(&mut self, timedelta: SimTimeDelta, process: ComponentId) {
         let time = self.curr_time + timedelta;
-        let event = ScheduledEvent { time, event: EventType::ProcessEvent(
-            ProcessEvent {
-                sender: process,
-                receiver: process,
-                event: Box::new(std::ptr::null::<usize>())
-            }
-        )};
-        // eprintln!("\t\t\tcreated event = {:?}", event);
-        self.events.push(event);
+        let event = ScheduledEvent {
+            time,
+            index: self.next_event,
+            event: EventType::ProcessEvent(
+                ProcessEvent {
+                    sender: process,
+                    receiver: process,
+                    event: Box::new(()),
+                }
+            ),
+        };
+        self.push_event(event);
     }
 
     pub fn sched_self_event1(&mut self, timedelta: SimTimeDelta, process: ComponentId, event: Box<dyn Any>) {
         let time = self.curr_time + timedelta;
-        let event = ScheduledEvent { time, event: EventType::ProcessEvent(
-            ProcessEvent {
-                sender: process,
-                receiver: process,
-                event: event,
-            }
-        )};
-        // eprintln!("\t\t\tcreated event = {:?}", event);
-        self.events.push(event);
+        let event = ScheduledEvent {
+            time,
+            index: self.next_event,
+            event: EventType::ProcessEvent(
+                ProcessEvent {
+                    sender: process,
+                    receiver: process,
+                    event: event,
+                }
+            ),
+        };
+        self.push_event(event);
     }
 
     pub fn sim_error(&mut self) {
         self.sim_status = SimStatus::Failure;
+    }
+
+    fn push_event(&mut self, event: ScheduledEvent) {
+        self.events.push(event);
+        self.next_event += 1;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_order1() {
+        const NUM_EV: usize = 5; // 3 does not work
+
+        let time = SimTime::default();
+        let process = ComponentId::new(1);
+        let mut sched = Scheduler::new();
+
+        let idx_to_val = |idx| { 2 * idx };
+
+        for idx in 0..NUM_EV {
+            let event1 = crete_event(time, process, idx, idx_to_val(idx));
+            sched.events.push(event1);
+        }
+
+        // ----------------------------------------
+
+        for idx in 0..NUM_EV {
+            // peak the next item and assert its index
+            let peaked_ev = sched.events.peek().unwrap();
+            assert_eq!(idx, peaked_ev.index);
+            eprintln!("peaked_ev = {:?}", peaked_ev);
+
+            // get the next item and assert its value
+            let popped_ev = sched.next_event();
+            assert!(matches!(popped_ev, EventType::ProcessEvent(_)));
+            let popped_num = unwrap_process_event(popped_ev);
+            assert_eq!(idx_to_val(idx), popped_num);
+            eprintln!("popped_ev = {:?}", popped_num);
+        }
+
+        assert!(matches!(sched.next_event(), EventType::EndSimulation));
+    }
+
+    fn unwrap_process_event(popped_ev: EventType) -> usize {
+        let ev = match popped_ev {
+            EventType::ProcessEvent(ev) => { Some(ev) },
+            _ => { None }
+        }.unwrap();
+        let num = ev.event.downcast::<usize>().unwrap();
+        *num
+    }
+
+    fn crete_event(time: SimTime, process: ComponentId, ev_idx: usize, ev_value: usize) -> ScheduledEvent {
+        ScheduledEvent {
+            time,
+            index: ev_idx,
+            event: EventType::ProcessEvent(
+                ProcessEvent {
+                    sender: process,
+                    receiver: process,
+                    event: Box::new(ev_value),
+                }
+            ),
+        }
     }
 }
